9c980dc3e1ebbd4cca6d7688df11396ac266d0f8
Bruce Han <intragerhan@gmail.com>
2026-07-31 21:22:02 +0900

    전략 패턴 - 예제1

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.template.code.AbstractTemplate;
 import hello.advanced.trace.template.code.SubClassLogic1;
 import hello.advanced.trace.template.code.SubClassLogic2;
@@ -60,10 +63,24 @@ public class ContextV1Test {
         AbstractTemplate template2 = new AbstractTemplate() {
             @Override
             protected void call() {
-                log.info("비즈니스 로직1 실행");
+                log.info("비즈니스 로직2 실행");
             }
         };
         log.info("클래스 이름2={}", template2.getClass());
         template2.execute();
+    }
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
     }
 }
