c8186132a2c4f17216e7f271c6f9de3e55ae09d1
Bruce Han <intragerhan@gmail.com>
2026-08-01 22:33:14 +0900

    JDK 동적 프록시 - 적용

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -4,6 +4,8 @@ import hello.advanced.config.AppV1Config;
 import hello.advanced.config.AppV2Config;
 import hello.advanced.config.v1_proxy.ConcreteProxyConfig;
 import hello.advanced.config.v1_proxy.InterfaceProxyConfig;
+import hello.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.advanced.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +16,9 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class)
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.app")
 public class AdvancedApplication {
 
